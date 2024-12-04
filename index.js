977ce201948aad9c0b0e18f9b8dfeaa784f4ca17
xyryc <amdanikzz@gmail.com>
2024-12-04 14:31:29 +0600

    added dynamic get method

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
     res.send(result);
   });
 
+  app.get("/equipment/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const query = { _id: new ObjectId(id) };
+    const result = await equipmentCollection.findOne(query);
+    res.send(result);
+  });
+
   // Send a ping to confirm a successful connection
   await client.db("admin").command({ ping: 1 });
   console.log("Pinged your deployment. You successfully connected to MongoDB!");
